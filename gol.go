c28549391a048f4c7e810b8f84df6d6b3a659ad9
AndyTheDragon <andre.jansam@gmail.com>
2025-11-29 23:35:41 +0100

    Enhance game functionality with topology switching and spaceship launching

--- gol.go
+++ gol.go
@@ -33,14 +33,67 @@ func NewGame(scale uint8, width, height int, randomizeProbability float32) *Game
 		buffer: NewTorusGrid(height, width),
 	}
 	g.grid.Randomize(randomizeProbability)
+	g.grid.CreateSpaceship("glider", -1, -1)
+	g.grid.CreateSpaceship("lightweight_spaceship", -1, -1)
 	return g
+}
+
+func (g *Game) setTopology(indexFunc indexFunc) {
+	g.grid = g.grid.CopyTo(indexFunc)
+	g.buffer = g.buffer.CopyTo(indexFunc)
 }
 
 func (g *Game) Update() error {
 	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
 		g.isPaused = !g.isPaused
 	}
-	
+
+	// topology switching
+    if inpututil.IsKeyJustPressed(ebiten.KeyDigit1) {
+		log.Println("Switching to Plane topology...")
+        g.setTopology(IndexPlane)     // plane
+    }
+    if inpututil.IsKeyJustPressed(ebiten.KeyDigit2) {
+		log.Println("Switching to Torus topology...")
+        g.setTopology(IndexTorus)     // torus
+    }
+    if inpututil.IsKeyJustPressed(ebiten.KeyDigit3) {
+		log.Println("Switching to Sphere topology...")
+        g.setTopology(IndexSphere)    // “sphere”
+    }
+    if inpututil.IsKeyJustPressed(ebiten.KeyDigit4) {
+		log.Println("Switching to Cylinder topology...")
+        g.setTopology(IndexCylinder)  // cylinder
+    }
+    if inpututil.IsKeyJustPressed(ebiten.KeyDigit5) {
+		log.Println("Switching to Klein bottle topology...")
+        g.setTopology(IndexKlein)     // Klein bottle
+    }
+	if inpututil.IsKeyJustPressed(ebiten.KeyDigit6) {
+		log.Println("Switching to Möbius strip topology...")
+		g.setTopology(IndexMoebiusX)  // Möbius strip (horizontal twist)
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyDigit7) {
+		log.Println("Switching to Reflective topology...")
+		g.setTopology(IndexReflect)    
+	}
+
+	// launch spaceships
+	if inpututil.IsKeyJustPressed(ebiten.KeyG) {
+		log.Println("Launching glider spaceship...")
+		g.grid.CreateSpaceship("glider", -1, -1)
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyS) {
+		log.Println("Launching lightweight spaceship...")
+		g.grid.CreateSpaceship("lightweight_spaceship", -1, -1)
+	}
+
+	// clear grid
+	 if inpututil.IsKeyJustPressed(ebiten.KeyC) {
+		log.Println("Clearing grid...")
+		g.grid.Clear()
+	}
+
 	if g.isPaused {
 		return nil
 	}
